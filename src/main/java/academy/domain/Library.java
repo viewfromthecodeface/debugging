6ae82da1da771538ad9d56515edc8616e7773c2a
Alan.Mellor <alan.mellor@bjss.com>
2018-11-20 22:16:48 +0000

    TECH DEBT Remove unused imports

--- src/main/java/academy/domain/Library.java
+++ src/main/java/academy/domain/Library.java
@@ -1,9 +1,8 @@
 package academy.domain;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 class Library {
     private List<Book> books;
